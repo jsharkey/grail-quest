a11c853c55ae4b350441e3c78a863586af7a0cc3
Jeff Sharkey <jsharkey@android.com>
2015-07-19 14:43:06 -0700

    Finally derived it!

--- derive-jul.py
+++ derive-jul.py
@@ -21,6 +21,7 @@ import sys, re, collections, math, numpy
 
 pts = collections.defaultdict(dict)
 
+"""
 for line in sys.stdin:
     if "==" in line:
         pt = line.strip()[3:]
@@ -30,7 +31,19 @@ for line in sys.stdin:
         frame = frame[-6:]
         pts[frame][pt] = (int(x),int(y))
 
-frames = sorted([ k for k in pts.keys() if len(pts[k]) == 4 ])
+"""
+
+A=[3307,2934,3299,2931,3329,2918,3312,2898,3310,2904,3336,2874,3348,2871,3333,2879,3326,2870,3346,2863,3324,2861,3325,2856,3340,2855,3308,2851,3340,2857,3337,2864,3335,2858,3330,2848,3322,2843,3335,2851,3317,2848,3307,2872,3312,2866,3326,2867,3331,2853,3298,2857,3317,2860,3318,2863,3315,2873,3330,2841,3335,2861,3333,2858,3338,2848,3333,2875,3300,2862,3291,2888,3296,2871,3311,2860,3284,2876,3324,2847,3313,2881,3321,2854,3306,2873,3304,2858,3303,2851,3311,2821,3322,2862,3332,2837,3293,2859,3327,2861,3320,2855,3313,2873,3302,2863,3300,2860,3318,2879,3312,2864,3312,2848,3314,2843,3281,2847,3323,2851,3274,2856,3310,2849,3290,2853,3310,2849,3308,2853,3268,2849,3271,2834,3289,2850,3291,2828,3299,2840,3321,2814,3322,2793,3278,2797,3319,2792,3312,2760,3262,2875,3294,2796]
+A=zip(A[::2], A[1::2])
+B=[675,2834,662,2838,705,2831,686,2811,687,2793,731,2763,748,2756,726,2760,715,2764,744,2757,711,2766,717,2740,739,2737,697,2718,739,2734,730,2751,729,2745,730,2707,719,2706,735,2714,712,2701,689,2738,699,2724,721,2711,732,2696,683,2706,711,2702,712,2705,704,2721,735,2675,737,2698,736,2689,744,2683,732,2706,689,2689,670,2713,679,2704,702,2696,663,2698,727,2665,700,2711,725,2655,698,2678,701,2657,703,2641,715,2626,724,2660,747,2620,685,2651,733,2651,727,2632,713,2652,696,2653,693,2651,718,2655,714,2636,719,2614,725,2600,676,2609,735,2610,660,2627,718,2598,687,2607,720,2591,713,2605,655,2606,664,2582,693,2576,699,2556,710,2559,744,2539,748,2521,686,2524,748,2507,742,2481,663,2549,712,2510,710,2660]
+B=zip(B[::2], B[1::2])
+
+for i in range(len(A)):
+    frame = "%02d.jpg" % (i)
+    pts[frame]['A'] = A[i]
+    pts[frame]['B'] = B[i]
+
+frames = sorted([ k for k in pts.keys() if len(pts[k]) == 2 ])
 
 def correct(params, pt):
     paramA,paramB,paramC,paramD=params
@@ -48,7 +61,34 @@ def correct(params, pt):
     deltaY = (y - centerY) / d
 
     dstR = math.sqrt(deltaX * deltaX + deltaY * deltaY)
-    srcR = ((paramA * dstR * dstR * dstR) + (paramB * dstR * dstR) + (paramC * dstR) + (paramD)) - dstR
+    srcR = ((paramA * math.pow(dstR, 3)) + (paramB * math.pow(dstR, 2)) + (paramC * math.pow(dstR, 1)) + (paramD)) - dstR
+
+    factor = abs(dstR / srcR);
+
+    srcXd = centerX + (deltaX * factor * d);
+    srcYd = centerY + (deltaY * factor * d);
+
+    return (srcXd, srcYd)
+
+def correct2(params, pt):
+    paramA,paramB,paramC,paramD=params
+    paramA=-paramA
+    paramB=-paramB
+
+    width=5760
+    height=3840
+    x,y = pt
+
+    d = min(width, height) / 2
+
+    centerX = (width) / 2.0
+    centerY = (height) / 2.0
+
+    deltaX = (x - centerX) / d
+    deltaY = (y - centerY) / d
+
+    dstR = math.sqrt(deltaX * deltaX + deltaY * deltaY)
+    srcR = ((paramA * math.pow(dstR, 3)) + (paramB * math.pow(dstR, 2)) + (paramC * math.pow(dstR, 1)) + (paramD)) - dstR
 
     factor = abs(dstR / srcR);
 
@@ -65,17 +105,24 @@ def dist(a, b):
 def correct_dist(params, a, b):
     return dist(correct(params, a), correct(params, b))
 
+def round_int(a):
+    x,y=a
+    return (int(x),int(y))
+
+
 def guess(params):
     res = collections.defaultdict(list)
     for frame in frames:
         pt = pts[frame]
+        # C is down in another plane
+        # B is far off on right side
         res['AB'].append(correct_dist(params, pt['A'], pt['B']))
         #res['AC'].append(correct_dist(params, pt['A'], pt['C']))
-        res['AD'].append(correct_dist(params, pt['A'], pt['D']))
+        #res['AD'].append(correct_dist(params, pt['A'], pt['D']))
         #res['BC'].append(correct_dist(params, pt['B'], pt['C']))
-        res['BD'].append(correct_dist(params, pt['B'], pt['D']))
+        #res['BD'].append(correct_dist(params, pt['B'], pt['D']))
         #res['CD'].append(correct_dist(params, pt['C'], pt['D']))
-    if max(res['AB']) < 1400: raise ValueError()
+    #if max(res['AB']) < 1400: raise ValueError()
     #print min(res['AB']), max(res['AB'])
     return [ numpy.std(res[k]) for k in sorted(res) ]
 
@@ -108,11 +155,37 @@ def search(params, index):
 
 
 
-params = [-0.1164, -0.0739, -0.6689, 0.0168]
+# ========================= r3, r2, r
+
+#params = [-0.1164, -0.0739, -0.6689, 0.0168]
+#params = [-0.06, -0.0879, 0, 0]
+#params = [-0.0467, -0.1192, 0, 0]
+
+#[0, 0, 0, 0] [10.061564166358261]  <-- worst case
+
+#[-0.09256000000000145, 0, 0, 0] [2.6666136482583762]
+#[-0.09256000000000145, -0.0013900000000046336, 0, 0] [2.666179691339674]
+#[-0.09198000000000167, -0.0013900000000046336, 0, 0] [2.6657489091518998]
+#[-0.09198, -0.00275, 0, 0] [2.6653302961767009]
+
+params = [-0.09198, -0.00275, 0, 0]
+
+a = (3336,2874)
+b = round_int(correct(params,a))
+c = round_int(correct2(params,b))
+
+print a
+print b
+print c
+
+
+
+exit()
+
 index = 0
 while True:
     params = search(params, index)
-    index = (index + 1) % 4
+    index = (index + 1) % 2
     print "== BEST SO FAR", params
 
 
--- proc.py
+++ proc.py
@@ -1,7 +1,7 @@
 
 import sys
 import collections
-import math
+import math, re
 
 # 0270 --> 10
 
@@ -15,10 +15,7 @@ def dist(a, b):
 
 
 def correct(pt):
-    paramA=-0.10705000
-    paramB=0
-    paramC=0
-    paramD=0
+    paramA,paramB,paramC,paramD = [-0.06, -0.0879, 0, 0]
 
     width=5760
     height=3840
@@ -56,27 +53,52 @@ def correct(pt):
 
 
 
-pts = collections.defaultdict(list)
+pts = collections.defaultdict(dict)
 
+"""
 for line in sys.stdin:
-    if len(line) < 5: continue
-
-    x, y, name = line.split("\t")
-    x = int(x)
-    y = int(y)
-
-    name = name.strip()
-    name = name[-6:]
+    if "==" in line:
+        pt = line.strip()[3:]
+        print "];\nvar set=[",
     
-    pts[name].append((x,y))
+    if "stage" in line:
+        x,y,frame = re.split("\s+", line.strip())
+        frame = frame[-6:]
+        pts[frame][pt] = (int(x),int(y))
+        print "[%s,%s]," % (x,y),
+        if "0.jpg" in frame: print "\n\t",
+
+print "];"
+
+frames = sorted([ k for k in pts.keys() if len(pts[k]) == 4 ])
+"""
+
+print "== STAGE TWO =="
+
+A=[3307,2934,3299,2931,3329,2918,3312,2898,3310,2904,3336,2874,3348,2871,3333,2879,3326,2870,3346,2863,3324,2861,3325,2856,3340,2855,3308,2851,3340,2857,3337,2864,3335,2858,3330,2848,3322,2843,3335,2851,3317,2848,3307,2872,3312,2866,3326,2867,3331,2853,3298,2857,3317,2860,3318,2863,3315,2873,3330,2841,3335,2861,3333,2858,3338,2848,3333,2875,3300,2862,3291,2888,3296,2871,3311,2860,3284,2876,3324,2847,3313,2881,3321,2854,3306,2873,3304,2858,3303,2851,3311,2821,3322,2862,3332,2837,3293,2859,3327,2861,3320,2855,3313,2873,3302,2863,3300,2860,3318,2879,3312,2864,3312,2848,3314,2843,3281,2847,3323,2851,3274,2856,3310,2849,3290,2853,3310,2849,3308,2853,3268,2849,3271,2834,3289,2850,3291,2828,3299,2840,3321,2814,3322,2793,3278,2797,3319,2792,3312,2760,3262,2875,3294,2796]
+A=zip(A[::2], A[1::2])
+print "var set=", A.__repr__().replace("(", "[").replace(")", "]"), ";"
+
+B=[675,2834,662,2838,705,2831,686,2811,687,2793,731,2763,748,2756,726,2760,715,2764,744,2757,711,2766,717,2740,739,2737,697,2718,739,2734,730,2751,729,2745,730,2707,719,2706,735,2714,712,2701,689,2738,699,2724,721,2711,732,2696,683,2706,711,2702,712,2705,704,2721,735,2675,737,2698,736,2689,744,2683,732,2706,689,2689,670,2713,679,2704,702,2696,663,2698,727,2665,700,2711,725,2655,698,2678,701,2657,703,2641,715,2626,724,2660,747,2620,685,2651,733,2651,727,2632,713,2652,696,2653,693,2651,718,2655,714,2636,719,2614,725,2600,676,2609,735,2610,660,2627,718,2598,687,2607,720,2591,713,2605,655,2606,664,2582,693,2576,699,2556,710,2559,744,2539,748,2521,686,2524,748,2507,742,2481,663,2549,712,2510,710,2660]
+B=zip(B[::2], B[1::2])
+print "var set=", B.__repr__().replace("(", "[").replace(")", "]"), ";"
+
+for i in range(len(A)):
+    frame = "%02d.jpg" % (i)
+    pts[frame]['A'] = A[i]
+    pts[frame]['B'] = B[i]
+
+frames = sorted([ k for k in pts.keys() if len(pts[k]) == 2 ])
+
+#exit()
 
 
-# we have points that should always be at same location
+#A=[ for x in ]
 
 
 # 1048,2681
 # 2964,2780
-
+"""
 x1 = 1048; y1 = 2681
 x2 = 2964; y2 = 2780
 
@@ -84,7 +106,7 @@ fx, fy = correct( (1048,2681) )
 #fx, fy = (1048,2681)
 
 defang = math.atan2(y2-y1, x2-x1) * 180 / math.pi
-
+"""
 
 f ="""
 5305    1182    file:///home/jsharkey/grail-quest/rt00.jpg
@@ -108,10 +130,22 @@ f ="""
 3739    1019    file:///home/jsharkey/grail-quest/rt75.jpg
 """
 
+f = """
+3215    972 file:///bandroid/lapse/100EOS5D/grail-quest/rt05.jpg
+3215    984 file:///bandroid/lapse/100EOS5D/grail-quest/rt15.jpg
+3239    998 file:///bandroid/lapse/100EOS5D/grail-quest/rt25.jpg
+3237    966 file:///bandroid/lapse/100EOS5D/grail-quest/rt35.jpg
+3208    1049    file:///bandroid/lapse/100EOS5D/grail-quest/rt45.jpg
+3197    1008    file:///bandroid/lapse/100EOS5D/grail-quest/rt55.jpg
+3236    1024    file:///bandroid/lapse/100EOS5D/grail-quest/rt65.jpg
+3211    1010    file:///bandroid/lapse/100EOS5D/grail-quest/rt75.jpg
+"""
+
 shift = {}
 
-import re
+defang = 1024
 
+"""
 for line in f.split("\n"):
     if len(line) < 5: continue
 
@@ -124,16 +158,56 @@ for line in f.split("\n"):
     
     shift[name] = (x,y)
     
-    x -= 3744; y -= 982;
-    print """convert rt%s -virtual-pixel black -distort ScaleRotateTranslate '0,0 1,1 0 %d,%d' xrt%s.jpg""" % (name, -x, -y, name)
+    x -= 3204; y -= 1005;
+    print "convert rt%s -virtual-pixel black -distort ScaleRotateTranslate '0,0 1,1 0 %d,%d' xrt%s.jpg" % (name, -x, -y, name)
+"""
+
+def rot(p, ang):
+    
+    x,y=p
+    print x,y, "--> ", ang,
+    
+    ang *= 0.0174532925
+    width=5760
+    height=3840
+
+    x-=width/2
+    y-=height/2
+
+    xx = (x*math.cos(ang)) - (y*math.sin(ang))
+    yy = (y*math.cos(ang)) + (x*math.sin(ang))
+    
+    xx+=width/2
+    yy+=height/2
+    
+    """
+    d = min(width, height) / 2
+    centerX = (width) / 2.0
+    centerY = (height) / 2.0
+    deltaX = (x - centerX) / d
+    deltaY = (y - centerY) / d
+
+    xx = (deltaX*math.cos(ang)) - (deltaY*math.sin(ang))
+    yy = (deltaY*math.cos(ang)) + (deltaX*math.sin(ang))
+
+    xx = centerX + (xx * d);
+    yy = centerY + (yy * d);
+    """
+
+    print "-->",  xx, yy
+    return (xx,yy)
+
+
 
 for p in sorted(pts):
     if not p.endswith("jpg"): continue
 
     #print p
 
-    x1, y1 = pts[p][0]
-    x2, y2 = pts[p][1]
+    #cx, cy = pts[p]['A']
+
+    x1, y1 = pts[p]['A']
+    x2, y2 = pts[p]['B']
     
     #print x1,y1
 
@@ -151,20 +225,21 @@ for p in sorted(pts):
 
     #xd = x1; yd = y1
 
-    #xd = x1 - fx; yd = y1 - fy
-    xd = 0; yd = 0
-    
-    if p in shift:
-        xd,yd = shift[p]
-        xd -= 3744; yd -= 982;
+    if defang == 1024:
+        defang = ang
+        fx = x1; fy = y1
+    ang -= defang
+
+    x1,y1 = rot((x1,y1),-ang)
 
     xd = 0; yd = 0
+    xd = x1 - fx; yd = y1 - fy
 
     #x1 -= 1048; y1 -= 2681
     #x2 -= 2964; y2 -= 2780
 
-    ang -= defang
-    ang += -2.71
+    #ang = 0
+    #ang += -2.71
 
     with open("proc%s.pp3" % (p), 'w') as f:
         f.write("""
@@ -189,7 +264,7 @@ yshift=%f
 Amount=0
 
 [LensProfile]
-LCPFile=/home/jsharkey/grail-quest/guess.lcp
+LCPFile=guess.lcp
 UseDistortion=true
 UseVignette=false
 UseCA=false
@@ -197,5 +272,5 @@ UseCA=false
 """ % (ang, -xd, -yd))
 
     orig = (int(p[0:2])+17)*10
-    print "~/rt_default_release_patched/rawtherapee -p proc%s.pp3 -Y -o rt%s -c ~/vacat/DCIM/101EOS5D/R59A%04d.CR2" % (p, p, orig)
+    print "~/rt_default_release_patched/rawtherapee -p proc%s.pp3 -Y -o rt%s -c /bandroid/lapse2/DCIM/101EOS5D/R59A%04d.CR2 " % (p, p, orig)
     #print """convert %s -virtual-pixel black -distort ScaleRotateTranslate '%d,%d 1,1 %f 1048,2681' -rotate 180 %s.post.jpg &""" % (p, x1, y1, ang, p)
